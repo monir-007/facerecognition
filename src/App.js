2c8012aa68fb3ef2542f93eeb084be9a8c7cd170
Monir Hossain <47362857+monir-007@users.noreply.github.com>
2020-12-20 01:37:36 +0600

    set upto API

--- src/App.js
+++ src/App.js
@@ -75,7 +75,24 @@ const particlesOptions = {
           detectRetina: true,
 }
 
-function App() {
+class App extends Component {
+
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+onSubmit = () => {
+  console.log('click');
+}
+
+render (){
   return (
     <div className="App">
        <Particles className="particles"
@@ -84,13 +101,18 @@ function App() {
       <Navigation/>
       <Logo/>
       <Rank />
-      <ImageLinkForm/>
+      <ImageLinkForm 
+      onInputText={this.onInputChange} 
+      onButtonSubmit={this.onSubmit}
+      />
       {/* {
       
       <FaceRecognition/>} */}
 
     </div>
   );
+}
+
 }
 
 export default App;
